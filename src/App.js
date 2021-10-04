0ebf8d830a4e80eda4cb96063ab4fb1669388694
ssahn0806 <ssahn0806@naver.com>
2021-10-04 11:27:55 +0900

    03 react's fundamental concepts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from "react";
+//import Potato from "./Potato";
+
+const Food = ({ fav }) => {
+  //console.log(props, "props");
+  //return <h3>I love {props.fav}</h3>;
+  return <h3>I love {fav}</h3>;
+};
 
 function App() {
   return (
     //<div className="App"/>
-   <div>Hello!!!!</div>
+    <div>
+      <h1>Hello</h1>
+      <Food
+        fav="kimchi"
+        // something={true}
+        // papapapa={["hello", 1, 2, 3, 4, true]}
+      />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
+    </div>
   );
 }
 
