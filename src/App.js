a68d7239f47358465341356ff02bfe9d93f359ea
ssahn0806 <ssahn0806@naver.com>
2021-10-04 12:11:39 +0900

    04 react's component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,75 @@
 import React from "react";
 //import Potato from "./Potato";
+import PropTypes from "prop-types";
 
-const Food = ({ fav }) => {
+const Food = ({ name, picture, rating }) => {
   //console.log(props, "props");
   //return <h3>I love {props.fav}</h3>;
-  return <h3>I love {fav}</h3>;
+  return (
+    <div>
+      <h2> I love {name}</h2>
+      <h4> {rating}/5.0</h4>
+      <img width={200} height={200} src={picture} alt={name} />
+    </div>
+  );
+};
+
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 3.8,
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.8,
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 3.2,
+  },
+];
+
+const renderFood = (dish) => {
+  return (
+    <Food
+      key={dish.id}
+      name={dish.name}
+      picture={dish.image}
+      rating={dish.rating}
+    />
+  );
 };
 
 function App() {
+  // console.log(foodILIke.map(renderFood));
   return (
     //<div className="App"/>
     <div>
       <h1>Hello</h1>
+      {/*
       <Food
         fav="kimchi"
         // something={true}
@@ -20,8 +78,15 @@ function App() {
       <Food fav="ramen" />
       <Food fav="samgiopsal" />
       <Food fav="chukumi" />
+      */}
+      {foodILike.map(renderFood)}
     </div>
   );
 }
 
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};
 export default App;
