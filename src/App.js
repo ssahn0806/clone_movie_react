b6291759f43f9dc49828302d486db1a4b7ae57a7
ssahn0806 <ssahn0806@naver.com>
2021-10-11 20:23:48 +0900

    06 Making Movie App

--- src/App.js
+++ src/App.js
@@ -1,117 +1,73 @@
 import React, { useState, useEffect } from "react";
-
-// // React.Component에 정의되어 있는 내용을 상속받아 사용가능 하게됨.
-// class App extends React.Component {
-//   /*State object - variant */
-//   state = {
-//     count: 0,
-//   };
-
-//   // React can't mutate state Directly.
-//   // By setState -> update State -> Rerender -> update visual source;
-//   // Targeting the update source - do not refresh all.
-
-//   add = () => {
-//     //console.log("add");
-//     // this.state.count++;
-//     // this.setState({ count: 1 });
-//     // this.setState({ count: this.state.count + 1 });
-//     // this.state.count = ReadOnly
-//     this.setState((current) => ({ count: current.count + 1 }));
-//   };
-
-//   minus = () => {
-//     //console.log("minus");
-//     //this.state.count--;
-//     // this.setState({ count: -1 });
-//     // this.setState({ count: this.state.count - 1 });
-//     this.setState((current) => ({ count: current.count - 1 }));
-//   };
-
-//   constructor(props) {
-//     super(props);
-//     console.log("This is constructor");
-//   }
-
-//   componentDidMount() {
-//     console.log("This is componentDidMount");
-//   }
-
-//   componentDidUpdate() {
-//     console.log("This is componentDidUpdate");
-//   }
-
-//   componentWillUnmount() {
-//     console.log("This is componentWillUnmount");
-//   }
-//   /* Class component returns JSX by render methods<상속에 의해 override> */
-//   render() {
-//     console.log("This is render");
-//     // return <h1>I'm a class component</h1>;
-//     // return <h1>The number is: {this.state.count}</h1>;
-//     return (
-//       <div>
-//         <h1>The number is: {this.state.count}</h1>
-//         {/*manipulate state value by button's event handler*/}
-//         <button onClick={this.add}>Add</button>
-//         <button onClick={this.minus}>Minus</button>
-//       </div>
-//     );
-//   }
-// }
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-
-//   const add = () => {
-//     setCount((prev) => prev + 1);
-//   };
-
-//   const minus = () => {
-//     setCount((prev) => prev - 1);
-//   };
-//   return (
-//     <div>
-//       <h1>The number is : {count} </h1>
-//       <button onClick={add}>Add</button>
-//       <button onClick={minus}>Minus</button>
-//     </div>
-//   );
-// };
-
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
 
+  getMovies = async () => {
+    // const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    // console.log(movies.data.data.movies, "data");
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+
+    // console.log(movies, "data");
+    // this.setState({ movies: movies });
+    this.setState({ movies, isLoading: false });
+  };
+
+  // getMovies = () => {
+  //   const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+
+  //   movies
+  //     .then((result) => console.log(result, "getMovie OK"))
+  //     .catch((err) => console.log(err, "getMovie Error"));
+  // };
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 5000);
+    // setTimeout(() => {
+    //   this.setState({ isLoading: false });
+    // }, 5000);
+
+    /* axios is async*/
+    this.getMovies();
   }
   render() {
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return (
-      <div>
+      <section class="container">
         {/* {this.state.isLoading ? "로딩중입니다..." : "로딩이 완료되었습니다."} */}
-        {isLoading ? "로딩중입니다..." : "로딩이 완료되었습니다."}
-      </div>
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">로딩중입니다...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map((movie) => {
+              // console.log(movie);
+              return (
+                <Movie
+                  key={`movie-${movie.id}`}
+                  // id={movie.id}
+                  title={movie.title}
+                  year={movie.year}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
     );
   }
 }
 
-// const App = () => {
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [movies, setMovies] = useState([]);
-
-//   useEffect(() => {
-//     console.log("component DidMount");
-//     setTimeout(() => {
-//       /* Data Loading */
-//       setIsLoading(false);
-//     }, 5000);
-//   }, []);
-//   return <div>{isLoading ? "로딩중입니다..." : "로딩이 완료되었습니다."}</div>;
-// };
 export default App;
