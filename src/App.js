9a800451ea3de0d36fa9c2dfdb17ed835bf28bcd
ssahn0806 <ssahn0806@naver.com>
2021-10-11 20:41:59 +0900

    07 Movie App Styling

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {/* {this.state.isLoading ? "로딩중입니다..." : "로딩이 완료되었습니다."} */}
         {isLoading ? (
-          <div class="loader">
-            <span class="loader__text">로딩중입니다...</span>
+          <div className="loader">
+            <span className="loader__text">로딩중입니다...</span>
           </div>
         ) : (
-          <div class="movies">
+          <div className="movies">
             {movies.map((movie) => {
               // console.log(movie);
               return (
@@ -60,6 +60,7 @@ class App extends React.Component {
                   year={movie.year}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />
               );
             })}
--- src/Movie.js
+++ src/Movie.js
@@ -1,14 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
-const Movie = ({ title, year, summary, poster }) => {
+const Movie = ({ title, year, summary, poster, genres }) => {
   return (
-    <div class="movie">
+    <div className="movie">
       <img src={poster} alt={title} title={title} />
-      <div class="movie__data">
-        <h3 class="movie__title">{title}</h3>
-        <h5 class="movie__year">{year}</h5>
-        <p class="movie_summary">{summary}</p>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => {
+            return (
+              <li className="movie__genre" key={index}>
+                {genre}
+              </li>
+            );
+          })}
+        </ul>
+        <p className="movie_summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
@@ -20,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
